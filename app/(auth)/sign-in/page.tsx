508c81c9baeb5b3ff12dfabd2c51261b07f946ed
John <johnmuller0910@gmail.com>
2025-05-20 22:59:29 +0300

    next up auth logic

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,86 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Page = () => {
-  return <div>Auth</div>;
+  return (
+    <main className={"sign-in"}>
+      <aside className={"testimonial"}>
+        <Link href={"/"}>
+          <Image
+            src={"/assets/icons/logo.svg"}
+            alt={"logo"}
+            width={32}
+            height={32}
+          />
+          <h1>SnapCast</h1>
+        </Link>
+
+        <div className={"description"}>
+          <section>
+            <figure>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <Image
+                  src={"/assets/icons/star.svg"}
+                  alt={"star"}
+                  width={20}
+                  height={20}
+                  key={index}
+                />
+              ))}
+            </figure>
+
+            <p>
+              SnapCast makes screen recording esay. From quick walkthrough to
+              full presentations, it&apos;s fast, smooth, and shareable in
+              seconds.
+            </p>
+            <article>
+              <Image
+                src={"/assets/images/jason.png"}
+                alt={"Jason"}
+                width={64}
+                height={64}
+                className={"rounded-full"}
+              />
+              <div>
+                <h2>JasonRivera</h2>
+                <p>Product designer at somewhere</p>
+              </div>
+            </article>
+          </section>
+        </div>
+
+        <p>© snapcast {new Date().getFullYear()}</p>
+      </aside>
+      <aside className={"google-sign-in"}>
+        <section>
+          <Link href={"/"}>
+            <Image
+              src={"/assets/icons/logo.svg"}
+              alt={"logo"}
+              width={40}
+              height={40}
+            />
+            <h1>snapCast</h1>
+          </Link>
+          <p>
+            Create and share your very fast <span>SnapCast video</span> in no
+            time!
+          </p>
+          <button>
+            <Image
+              src={"/assets/icons/google.svg"}
+              alt={"google"}
+              width={22}
+              height={22}
+            />
+            <span>Sign in with Google</span>
+          </button>
+        </section>
+      </aside>
+      <div className={"overlay"} />
+    </main>
+  );
 };
 export default Page;
