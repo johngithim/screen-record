8d334f9318ccbdc1d0c2dc4593e725fcf0cbe7bc
John <johnmuller0910@gmail.com>
2025-05-20 22:34:20 +0300

    next up Auth UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
+import Image from "next/image";
+import { userInfo } from "node:os";
+import { dummyCards } from "@/constants";
 
 const Page = () => {
   return (
     <main className={"wrapper page"}>
       <Header title={"All videos"} subHeader={"public library"} />
-      <h1 className={"font-karla"}>Welcome</h1>
+      <section className={"video-grid"}>
+        {dummyCards.map((card) => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
     </main>
   );
 };
